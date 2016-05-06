4cef4f7310609d49029abfac150539befbdd3b08
bufan <bufanjs@gmail.com>
2016-05-06 22:55:43 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        $: true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-undef": 2,
+        "no-unused-vars": 2
+    }
+};
